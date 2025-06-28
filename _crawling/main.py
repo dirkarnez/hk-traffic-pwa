7d1967cd70923a6a9020b3310cbbcf24e75b228e
Dirk Arnez <smalldirkalex@gmail.com>
2025-06-28 17:50:43 +0800

    - add _crawling/main.py

---
+++ _crawling/main.py
@@ -0,0 +1,8 @@
+import json
+import re
+import time
+
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
